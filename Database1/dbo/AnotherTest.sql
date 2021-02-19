bc467dc4f63c6b759f0a3a4d321adcdba13866b4
Daniel Ware <ware.daniel.a@gmail.com>
2021-02-18 19:29:00 -0600

    basic docker support for ssdt

---
+++ Database1/dbo/AnotherTest.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[AnotherTest]
+(
+    [Id] INT NOT NULL PRIMARY KEY,
+    [Thing] VARCHAR(256) NULL
+)
---
+++ Database1/dbo/User.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[User]
+(
+    [Id]        INT NOT NULL PRIMARY KEY,
+    [Name]      VARCHAR(256) NULL,
+    [AnotherName] VARCHAR(512) NULL
+)
